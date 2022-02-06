6078bb45c36a952c39230986d9118b01f0963712
emresaridogan <emresaridogan@outlook.com>
2022-02-06 14:46:09 +0300

    project images

--- YMimarisi/KartGoruntule.Designer.cs
+++ YMimarisi/KartGoruntule.Designer.cs
@@ -62,54 +62,55 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Font = new System.Drawing.Font("Molde Condensed Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(33, 219);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(180, 18);
+            this.label4.Size = new System.Drawing.Size(203, 33);
             this.label4.TabIndex = 4;
             this.label4.Text = "Yunus Emre Sarıdoğan";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label5.Font = new System.Drawing.Font("Molde Condensed Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(364, 221);
+            this.label5.Location = new System.Drawing.Point(362, 221);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 26);
+            this.label5.Size = new System.Drawing.Size(74, 19);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Son kullanım\r\n      tarihi";
+            this.label5.Text = "VALID THRUE";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Font = new System.Drawing.Font("Molde Condensed Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(374, 248);
+            this.label6.Location = new System.Drawing.Point(381, 242);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 19);
+            this.label6.Size = new System.Drawing.Size(41, 19);
             this.label6.TabIndex = 6;
             this.label6.Text = "06/26";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Molde Condensed Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(440, 236);
+            this.label7.Location = new System.Drawing.Point(443, 221);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.Size = new System.Drawing.Size(30, 19);
             this.label7.TabIndex = 7;
-            this.label7.Text = "CVV :";
+            this.label7.Text = "CVV";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Font = new System.Drawing.Font("Molde Condensed Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(440, 252);
+            this.label8.Location = new System.Drawing.Point(443, 242);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.Size = new System.Drawing.Size(30, 19);
             this.label8.TabIndex = 8;
             this.label8.Text = "894";
             // 
